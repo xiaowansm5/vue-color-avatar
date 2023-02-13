2b932d9768a5823613f51c3dbf5446e472863c1c
xiaowansm <31873594+xiaowansm5@users.noreply.github.com>
2023-02-13 19:12:11 +0800

    Update Header.vue

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -2,11 +2,11 @@
   <header class="header">
     <Logo />
 
-    <h2 class="site-title">Color Avatar</h2>
+    <h2 class="site-title">头像生成</h2>
 
     <div class="header-right">
       <a
-        href="https://github.com/Codennnn/vue-color-avatar"
+        href="https://github.com/xiaowansm5"
         target="_blank"
         rel="nofollow noopener noreferrer"
       >
